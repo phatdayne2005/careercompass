a580423688305e817d1e8a9e75e7704c5565e831
vmquan2200 <168800095+vmquan2200@users.noreply.github.com>
2026-06-30 22:05:44 +0700

    Add Front-end

--- src/main/java/vn/uth/careercompass/kernel/repository/UserSkillRepository.java
+++ src/main/java/vn/uth/careercompass/kernel/repository/UserSkillRepository.java
@@ -3,6 +3,7 @@ package vn.uth.careercompass.kernel.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import vn.uth.careercompass.admin.entity.Skill;
 import vn.uth.careercompass.kernel.entity.User;
 import vn.uth.careercompass.kernel.entity.UserSkill;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @Repository
 public interface UserSkillRepository extends JpaRepository<UserSkill, Long> {
     List<UserSkill> findByUser(User user);
+    boolean existsByUserAndSkill(User user, Skill skill);
     @Transactional
     void deleteByUser(User user);
 }
